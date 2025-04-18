2ec6971951e027f12ba6ba9d971e6c1a89077df5
Paulo Sérgio <paulosergiosrs08@gmail.com>
2025-04-18 17:55:41 -0300

    feat:  added lessons of OOP

--- src/br/com/screenmatch/Main.java
+++ src/br/com/screenmatch/Main.java
@@ -4,7 +4,11 @@ import br.com.screenmatch.filme.Filme;
 
 public class Main {
     public static void main(String[] args) {
-        Filme meuFilme = new Filme();
+        Filme meuFilme = new Filme("Top GUN", 1990,false, 199);
 
+        meuFilme.avaliarFilme(5.0);
+        meuFilme.avaliarFilme(3.9);
+        meuFilme.avaliarFilme(5.0);
+        System.out.println(meuFilme.exibirfichaTecnica());
     }
 }
--- src/br/com/screenmatch/filme/Filme.java
+++ src/br/com/screenmatch/filme/Filme.java
@@ -1,12 +1,43 @@
 package br.com.screenmatch.filme;
 
 public class Filme {
-    public String nome;
-    public int anoDeLancamento;
-    public boolean incluidoNoPlano;
-    public double avaliacao;
-    public int totalAvaliacoes;
-    public int duracaoEmMinutos;
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalAvaliacoes;
+    private int duracaoEmMinutos;
+
+    public Filme( String nome, int anoDeLancamento, boolean incluidoNoPlano, int duracaoEmMinutos){
+        this.nome = nome;
+        this.anoDeLancamento = anoDeLancamento;
+        this.incluidoNoPlano = incluidoNoPlano;
+        this.somaDasAvaliacoes = 0;
+        this.totalAvaliacoes = 0;
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public String exibirfichaTecnica(){
+        return String.format("""
+               
+               Nome: %s 
+               Ano de lançamento: %d
+               Incluído no plano: %b
+               Avaliação: %.1f
+               Total de Avaliações: %d
+               Duração em minutos: %d
+               Média de valiações: %.1f
+               
+               """, this.nome, this.anoDeLancamento, this.incluidoNoPlano, this.somaDasAvaliacoes, this.totalAvaliacoes, this.duracaoEmMinutos, mediaAvaliacoes());
+    }
 
 
+    public void avaliarFilme(double notaFilme){
+        this.somaDasAvaliacoes += notaFilme;
+        this.totalAvaliacoes++ ;
+    }
+
+    public double mediaAvaliacoes(){
+        return this.somaDasAvaliacoes / this.totalAvaliacoes;
+    }
 }
